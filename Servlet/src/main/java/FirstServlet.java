1384c7a243ff23075fab354d7d890f1ce457367a
jit12 <jit12@192.168.0.100>
2022-07-23 23:33:36 +0530

    commit

---
+++ Servlet/src/main/java/FirstServlet.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;  
+ 
+  
+public class FirstServlet extends HttpServlet {  
+public void doGet(HttpServletRequest request, HttpServletResponse response){  
+        try{  
+  
+        response.setContentType("text/html");  
+        PrintWriter out = response.getWriter();  
+          
+        String n=request.getParameter("userName");  
+        out.print("Welcome "+n);  
+          
+        //creating form that have invisible textfield  
+        out.print("<form action='servlet2'>");  
+        out.print("<input type='hidden' name='uname' value='"+n+"'>");  
+        out.print("<input type='submit' value='go'>");  
+        out.print("</form>");  
+        out.close();  
+  
+                }catch(Exception e){System.out.println(e);}  
+    }  
+  
+}  
---
+++ Servlet/src/main/java/SecondServlet.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;  
+ 
+public class SecondServlet extends HttpServlet {  
+public void doGet(HttpServletRequest request, HttpServletResponse response){ 
+        try{  
+        response.setContentType("text/html");  
+        PrintWriter out = response.getWriter();  
+          
+        //Getting the value from the hidden field  
+        String n=request.getParameter("uname");  
+        out.print("Hello "+n);  
+  
+        out.close();  
+                }
+    catch(Exception e){System.out.println(e);}  
+    }  
+}  
